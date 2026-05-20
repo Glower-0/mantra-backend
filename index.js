784d732a5b0b321ba2197e38ba17f862376b1a0a
Julio Milan <milan.ewok@gmail.com>
2026-05-19 18:48:55 -0600

    Refactor PostgreSQL connection setup with env variables
    Updated PostgreSQL connection configuration to use environment variables for database credentials.

--- index.js
+++ index.js
@@ -16,9 +16,13 @@ app.use(express.static(path.join(__dirname)));
 // 2. Configuración de conexión a PostgreSQL usando variables de entorno
 // Render inyectará process.env.DATABASE_URL automáticamente en producción
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   ssl: {
-    rejectUnauthorized: false // Obligatorio para que Render no bloquee el acceso por certificados SSL
+    rejectUnauthorized: false
   }
 });
 
