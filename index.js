d3ae93ef17683959ba880a07e9f8cacc65d1b619
Julio Milan <milan.ewok@gmail.com>
2026-05-22 16:51:25 -0600

    Refactor index.js to include Cloudinary integration

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const { Pool } = require('pg');
 require('dotenv').config();
-
+const cloudinary = require('cloudinary').v2;
 const app = express();
 const PORT = process.env.PORT || 3000;
 
